af66455d4bb3156468b2b517908ff181abfcfb68
Adith Nambiar <adiadithdnambiar@gmail.com>
2026-05-12 11:44:44 +0530

    New1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,19 +4,19 @@ const projects = [
   {
     title: "E-Learning App",
     desc: "Built an E-Learning App for Solution Ed-Tech and Tuitions. Built this app with 2 portals for admin and students",
-    image: "/project1.png",
+    image: "/Project1.png",
     link: "https://www.linkedin.com/posts/adith-d-nambiar-498b53247_freelance-developer-edtech-ugcPost-7456968407217774592-BbSF?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD03X_QBy3oYXTugCV7zoGXkyXOmnww28VU",
   },
   {
     title: "E-Commerce App",
     desc: "E-Commerce app where users can see the listed items and order them while admin can add and manage them.",
-    image: "/project2.png",
+    image: "/Project2.png",
     link: "https://www.linkedin.com/posts/adith-d-nambiar-498b53247_fullstack-reactjs-nodejs-ugcPost-7449485886486114306-CNmu?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD03X_QBy3oYXTugCV7zoGXkyXOmnww28VU",
   },
   {
     title: "Social Media Platform",
     desc: "A social media app where users can post, like comment, follow and chat.",
-    image: "/project3.png",
+    image: "/Project3.png",
     link: "https://www.linkedin.com/posts/adith-d-nambiar-498b53247_instagram-facebook-buildinpublic-ugcPost-7442147419678568449-BCgy?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD03X_QBy3oYXTugCV7zoGXkyXOmnww28VU",
   },
 ];
